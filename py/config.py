2065292092159a2e0ff362de8c0a681fde8a8af6
Vladislave023 <148541641+Vladislave023@users.noreply.github.com>
2025-04-10 14:31:34 +1000

    Update config.py

--- py/config.py
+++ py/config.py
@@ -1,11 +1,13 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()  
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
-TOKEN = os.getenv("BOT_TOKEN")  
-ADMIN_ID = os.getenv("ADMIN_ID")
+TOKEN = os.getenv("BOT_TOKEN") or os.environ.get("BOT_TOKEN")  # Берёт из .env или GitHub Secrets
+ADMIN_ID = int(os.getenv("ADMIN_ID") or os.environ.get("ADMIN_ID"))  # Преобразует в число
+
 WEBSITE_URL = "https://everest-tk.ru/obnovlenie-kataloga"
 CHANNEL_URL = "https://t.me/everesttkk"
 WORKING_HOURS_TEXT = "Ожидайте, в рабочее время с 9 до 18 по Хабаровску с вами свяжется специалист"
-ALBUM_THRESHOLD_SECONDS = 1.5  
+ALBUM_THRESHOLD_SECONDS = 1.5
